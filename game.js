bb22adb662b22bc941c177cd608920fc192839f0
ringuemkt-rgb <ringuemkt@gmail.com>
2026-02-19 19:16:11 -0300

    Add basic Phaser game setup in game.js

---
+++ game.js
@@ -0,0 +1,35 @@
+// game.js
+
+// Basic Phaser game setup placeholder
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    // Load assets here
+}
+
+function create() {
+    // Initialize game objects here
+}
+
+function update() {
+    // Game loop updates here
+}
