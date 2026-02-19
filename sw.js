e0f403bb00bf3484955fd8af7cc0e871c2692f66
ringuemkt-rgb <ringuemkt@gmail.com>
2026-02-19 19:16:46 -0300

    Create sw.js for Service Worker offline support

---
+++ sw.js
@@ -0,0 +1,49 @@
+// sw.js - Service Worker for Offline Support
+
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+    '/index.html',
+    '/styles.css',
+    '/script.js'
+];
+
+// Install event - cache resources
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event - serve cached content
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response from cached version
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
