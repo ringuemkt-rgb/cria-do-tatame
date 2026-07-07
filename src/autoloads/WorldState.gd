e4e7e3e8324163930d9b025e38e34dad66d09c78
Capoeira de Cria <ringuemkt@gmail.com>
2026-07-07 01:17:49 -0300

    Add combat result to WorldState

--- src/autoloads/WorldState.gd
+++ src/autoloads/WorldState.gd
@@ -2,7 +2,7 @@ extends Node
 
 var week := 1
 var day_index := 0
-var days := ["segunda", "terça", "quarta", "quinta", "sexta", "sábado", "domingo"]
+var days := ["segunda", "terca", "quarta", "quinta", "sexta", "sabado", "domingo"]
 var act := 1
 var money := 0
 var energy := 100
@@ -10,14 +10,8 @@ var player_id := "ruan_macacao"
 var current_hub := "terreiro_da_luta"
 var unlocked_skills := []
 var completed_missions := []
-var reputation := {
-	"honra": 0,
-	"hype": 0,
-	"sombra": 0,
-	"legado": 0,
-	"dupla_face": 0,
-	"moral": 50
-}
+var last_combat_result := {}
+var reputation := {"honra": 0, "hype": 0, "sombra": 0, "legado": 0, "dupla_face": 0, "moral": 50}
 
 func reset_new_game():
 	week = 1
@@ -29,21 +23,11 @@ func reset_new_game():
 	current_hub = "terreiro_da_luta"
 	unlocked_skills = []
 	completed_missions = []
+	last_combat_result = {}
 	reputation = {"honra": 0, "hype": 0, "sombra": 0, "legado": 0, "dupla_face": 0, "moral": 50}
 
 func to_dict():
-	return {
-		"week": week,
-		"day_index": day_index,
-		"act": act,
-		"money": money,
-		"energy": energy,
-		"player_id": player_id,
-		"current_hub": current_hub,
-		"unlocked_skills": unlocked_skills,
-		"completed_missions": completed_missions,
-		"reputation": reputation
-	}
+	return {"week": week, "day_index": day_index, "act": act, "money": money, "energy": energy, "player_id": player_id, "current_hub": current_hub, "unlocked_skills": unlocked_skills, "completed_missions": completed_missions, "last_combat_result": last_combat_result, "reputation": reputation}
 
 func load_from_dict(data):
 	week = data.get("week", 1)
@@ -55,4 +39,5 @@ func load_from_dict(data):
 	current_hub = data.get("current_hub", "terreiro_da_luta")
 	unlocked_skills = data.get("unlocked_skills", [])
 	completed_missions = data.get("completed_missions", [])
+	last_combat_result = data.get("last_combat_result", {})
 	reputation = data.get("reputation", reputation)
