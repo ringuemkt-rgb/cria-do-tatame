a600fd435944538792a506b2b315ce1706393c8f
Capoeira de Cria <ringuemkt@gmail.com>
2026-07-07 01:15:29 -0300

    Connect main menu to hub scene

--- scenes/main_menu/MainMenu.gd
+++ scenes/main_menu/MainMenu.gd
@@ -1,11 +1,19 @@
 extends Control
 
+const HUB_SCENE := "res://scenes/hubs/TerreiroDaLuta.tscn"
+
 func _ready() -> void:
 	$MenuButtons/NewGame.pressed.connect(_on_new_game_pressed)
 	$MenuButtons/Continue.pressed.connect(_on_continue_pressed)
 
 func _on_new_game_pressed() -> void:
-	print("new career")
+	WorldState.reset_new_game()
+	SaveManager.save_game(1)
+	get_tree().change_scene_to_file(HUB_SCENE)
 
 func _on_continue_pressed() -> void:
-	print("continue career")
+	if SaveManager.has_save(1):
+		SaveManager.load_game(1)
+	else:
+		WorldState.reset_new_game()
+	get_tree().change_scene_to_file(HUB_SCENE)
