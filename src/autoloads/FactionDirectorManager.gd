0f2a743c450ba3261f630ee544aa50362e69f25a
Capoeira de Cria <ringuemkt@gmail.com>
2026-07-24 06:01:23 -0300

    fix(compat): restaurar caminho público do FactionDirector como wrapper v3

---
+++ src/autoloads/FactionDirectorManager.gd
@@ -0,0 +1,5 @@
+extends "res://src/autoloads/FactionDirectorV3.gd"
+
+## Caminho público preservado para cenas, validadores e saves antigos.
+## Toda implementação ativa vive em FactionDirectorV3.gd e contém somente
+## LEM, NTM e ALE.
